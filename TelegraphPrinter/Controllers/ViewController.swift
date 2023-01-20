f1042ef934281967fe9018e3c22f889a212bd7e7
Airat K <kamairat@icloud.com>
2023-01-20 19:51:21 +0300

    Add sort button, fix bugs

--- TelegraphPrinter/Controllers/ViewController.swift
+++ TelegraphPrinter/Controllers/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: NSViewController {
     @IBOutlet var inputView: NSTextView!
     @IBOutlet var outputView: NSTextView!
     @IBOutlet var progressCircle: NSProgressIndicator!
+    @IBOutlet weak var sortButton: NSButton!
     
     private let telegraphClient = TelegraphService()
     
@@ -21,11 +22,13 @@ class ViewController: NSViewController {
         super.viewDidLoad()
     }
     
-    
-    @IBAction func printAction(_ sender: Any) {
+    @IBAction func print(_ sender: Any) {
         self.dropOutputViewContent()
         
         guard !self.inputView.string.isEmpty else { return }
+        
+        self.progressCircle.startAnimation(sender)
+        self.sortButton.isEnabled = false
         
         let pagesRawContent = self.getPagesRawContentFromInputView()
         
@@ -34,37 +37,34 @@ class ViewController: NSViewController {
             
             guard !content.isEmpty else { continue }
             
-            self.progressCircle.startAnimation(sender)
             self.telegraphClient.createPage(title: title, content: content) { (page, error) in
-                if index == pagesRawContent.count - 1 {
-                    self.progressCircle.stopAnimation(sender)
-                }
-                
                 if let error = error {
                     self.appendOutputViewContent(withString: "Ошибка в обработке вопроса \"\(title)\": \(error)")
+                    self.stopProgessAnimationOnLastArticle(currentIndex: index, totalEntitiesNumber: pagesRawContent.count, sender: sender)
                     return
                 }
                 
                 self.appendOutputViewContent(withString: self.telegraphClient.telegraphUrl + page!.path)
+                self.stopProgessAnimationOnLastArticle(currentIndex: index, totalEntitiesNumber: pagesRawContent.count, sender: sender)
             }
         }
     }
     
+    @IBAction func sort(_ sender: Any) {
+        let lines = self.outputView.string.split(separator: "\n")
+        let sortedLines = lines.sorted {
+            self.getQuestionNumberFromUrl(String($0)) < self.getQuestionNumberFromUrl(String($1))
+        }
+        
+        self.outputView.string = sortedLines.joined(separator: "\n")
+    }
     
     private func dropOutputViewContent() {
         self.outputView.string = ""
     }
     
     private func getPagesRawContentFromInputView() -> [String] {
-        var pagesContent = self.inputView.string.split(regex: "\n\n[\t]*[0-9]+.[\t]*")
-        
-        pagesContent[0] = pagesContent[0].split(regex: "^[\t]*[0-9]+.[\t]*").last!
-        
-        return pagesContent
-    }
-    
-    private func appendOutputViewContent(withString string: String) {
-        self.outputView.string = self.outputView.string.appending(string + "\n")
+        return self.inputView.string.split(regex: "\n\n\n[\n]*")
     }
     
     private func getPageEntities(from pageRawContent: String) -> (title: String, content: [[String : Any]]) {
@@ -79,6 +79,24 @@ class ViewController: NSViewController {
         }
         
         return (title, content)
+    }
+    
+    private func appendOutputViewContent(withString string: String) {
+        self.outputView.string = self.outputView.string.appending(string + "\n")
+    }
+    
+    private func stopProgessAnimationOnLastArticle(currentIndex: Int, totalEntitiesNumber: Int, sender: Any) {
+        guard (currentIndex + 1) >= totalEntitiesNumber else { return }
+        
+        self.progressCircle.stopAnimation(sender)
+        self.sortButton.isEnabled = true
+    }
+    
+    private func getQuestionNumberFromUrl(_ urlString: String) -> Int {
+        let questionTitle = urlString.trimmingCharacters(in: CharacterSet(charactersIn: telegraphClient.telegraphUrl))
+        let rawQuestiongNumber = questionTitle.split(separator: "-", maxSplits: 1).first!
+        
+        return Int(rawQuestiongNumber) ?? Int.max
     }
 
 }
